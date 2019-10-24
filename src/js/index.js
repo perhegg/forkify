58085b737657c2b87b275a41af01bdacffea281a
Per Eriksson <per_hegg@hotmail.com>
2019-10-24 11:44:59 +0200

    add getResults from api function

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,16 @@
 // Global app controller
-// 1992bf39ce325f0e2fe71135bd717e7d
+// 
 // https://www.food2fork.com/api/search
-
-async function getResults (query) {
-    
+import axios from 'axios';
+async function getResults(query) {
+    try {
+        const key = '1992bf39ce325f0e2fe71135bd717e7d';
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`)
+        const recipes = res.data.recipes
+        console.log(recipes)
+    }
+    catch(error) {
+        alert(error)
+    }
 }
-getResults();  
+getResults('pizza');  
