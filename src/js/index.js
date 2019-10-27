9c2a9b53709aa9ca6b484d70ef09e396ad0f9bb2
Per Eriksson <per_hegg@hotmail.com>
2019-10-27 17:00:37 +0100

    adds likes modell, controller and view

--- src/js/index.js
+++ src/js/index.js
@@ -1,62 +1,68 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
+import * as likesView from './views/likesView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
  * - Search object
- * - Current recipe
+ * - Current recipe object
  * - Shopping list object
  * - Liked recipes
  */
+const state = {};
 
-const state = {
-
-}
-
-/**
+/** 
  * SEARCH CONTROLLER
  */
-
 const controlSearch = async () => {
-    //1 Get query from view
+    // 1) Get query from view
     const query = searchView.getInput();
 
-    if(query) {
-        // 2 New search object and add to state
+    if (query) {
+        // 2) New search object and add to state
         state.search = new Search(query);
 
-        // 3 Prepare UI for results
-        searchView.clearInput()
-        searchView.clearResults()
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
         renderLoader(elements.searchRes);
-        // 4 Search for recipes 
-        await state.search.getResults();
-        clearLoader();
+
+        try {
+            // 4) Search for recipes
+            await state.search.getResults();
     
-        // 5 Render results on UI
-        searchView.renderResults(state.search.result);
-    }   
+            // 5) Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (err) {
+            alert('Something wrong with the search...');
+            clearLoader();
+        }
+    }
 }
 
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
-})
+});
+
 
 elements.searchResPages.addEventListener('click', e => {
-    const btn = e.target.closest('.btn-inline')
+    const btn = e.target.closest('.btn-inline');
     if (btn) {
         const goToPage = parseInt(btn.dataset.goto, 10);
-        searchView.clearResults()
+        searchView.clearResults();
         searchView.renderResults(state.search.result, goToPage);
-        console.log(goToPage);
     }
-})
+});
 
-/**
+
+/** 
  * RECIPE CONTROLLER
  */
 const controlRecipe = async () => {
@@ -96,8 +102,9 @@ const controlRecipe = async () => {
         }
     }
 };
-
+ 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
 
 /** 
  * LIST CONTROLLER
@@ -131,6 +138,58 @@ elements.shopping.addEventListener('click', e => {
         state.list.updateCount(id, val);
     }
 });
+
+
+/** 
+ * LIKE CONTROLLER
+ */
+const controlLike = () => {
+    if (!state.likes) state.likes = new Likes();
+    const currentID = state.recipe.id;
+
+    // User has NOT yet liked current recipe
+    if (!state.likes.isLiked(currentID)) {
+        // Add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        );
+        // Toggle the like button
+        likesView.toggleLikeBtn(true);
+
+        // Add like to UI list
+        likesView.renderLike(newLike);
+
+    // User HAS liked current recipe
+    } else {
+        // Remove like from the state
+        state.likes.deleteLike(currentID);
+
+        // Toggle the like button
+        likesView.toggleLikeBtn(false);
+
+        // Remove like from UI list
+        likesView.deleteLike(currentID);
+    }
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+};
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+    
+    // Restore likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
+
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
